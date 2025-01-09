11d25344c9214d699deb90850a3f46c51f62de49
Ana Maria <naoseidenada0418@gmail.com>
2025-01-09 11:35:13 +0100

    ejercicio 2.12 array

--- js/task.js
+++ js/task.js
@@ -8,14 +8,25 @@ const tasks = [
       id: 4,
     },
 ];
-const btn = document.querySelector('.js-btn');
+console.log(tasks);
 
-for (const tasks of tasks) {
-  tasks.innerHTML += `<li> ${tasks} </li>`;
-  console.log(tasks);
+const btnAdd = document.querySelector(".js-btn-add");
+const textAdd = document.querySelector(".js-text-task-add");
+const inputTextFilter = document.querySelector(".js-text-task-filter");
+const btnFilter = document.querySelector(".js-btn-filter");
+
+for (const task of tasks) {
+    const taskPepino = document.createElement("li");//crea un elemento li y lo guarda en la variable taskPepino.
+    taskPepino.innerHTML = task.name;//añade el contenido (los nombres que estan en el array) a la variable taskPepino.
+    taskPepino.classList.add("task");//el .classList.add, añade la clase task a la variable taskPepino, eso permite añadir classes css a los <li>.
+    taskPepino.setAttribute("id", task.id);
+    if (task.completed) {
+        taskPepino.classList.add("task--completed");
+    }
+    document.querySelector(".js-tasks").appendChild(taskPepino); //esta función anexa el elemento creado (li) que nombramos como taskPepino al js-tasks para que se pinte en la web.
 }
 
-renderTasks();
-btn.addEventListener('click', handleClick);
+//taskPepino.setAttribute("id", task.id); Este fragmento de código establece un atributo id y agrega condicionalmente una clase CSS al elemento <li> (taskPepino):
 
-
+//taskPepino.setAttribute("id", task.id): establece el atributo id del elemento taskPepino en el id de la tarea actual.
+//if (task.completed) { taskPepino.classList.add("task--completed"); }: verifica si la tarea está completa. Si es así, agrega la clase CSS task--completed al elemento taskPepino. Esto le permite aplicar estilos específicos a las tareas completadas.
